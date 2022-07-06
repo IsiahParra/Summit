31b63010544006e616440dd42086203839b937d8
Isiah Parra <isiahparra@Isiahs-MacBook-Pro.local>
2022-07-05 23:14:55 -0600

    Storyboard updated and user model finished

--- Summit/Models/User/User.swift
+++ Summit/Models/User/User.swift
@@ -6,27 +6,32 @@
 //
 
 import Foundation
-//
-//class User { // conform to equatable for deletion perhaps?
-//    
-//    enum Key {
-//        static var userName = "userName"
-//        static let uid = "uid"
+
+class User { // conform to equatable for deletion perhaps?
+    
+    enum Key {
+        static var userName = "userName"
+        static let id = "id"
 //        static var trails = "trails"
 //        static var hikes = "hikes"
-//    }
-//    
-//    //MARK: Properties
-//    var userName: String
-//    var trails: [String] = []
-//    var hikes: [String] = []
-//    
-//    init( userName: String, trails: [String], hikes: [String]) {
-//        self.userName = userName
-//        self.trails = trails
-////        self.hikes = hikes
-//    }
-//   
-//        
-//
-//}
+        static var fireUserID = "fireUserID"
+    }
+    
+    //MARK: Properties
+    var userName: String
+    var id: String
+    var fireUserID: String
+    //MARK: not saved in firestore, it has to be fetched seperatly
+    //    var trails: [Trail] = [] // do not save to firestore
+    //    var hikes: [Hike] = [] //  do not save to firestore
+    
+//    init( userName: String, trails: [Trail], hikes: [Hike], id: String, fireUserID: String) {
+    init( userName: String, id: String, fireUserID: String){
+        self.userName = userName
+        self.id = id
+        self.fireUserID = fireUserID
+    }
+   
+        
+
+}
