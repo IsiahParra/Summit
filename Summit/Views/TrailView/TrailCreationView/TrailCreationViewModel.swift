e7a0d341f3ba0ff160c191b0227f66cd09ad2149
Isiah Parra <isiahparra@Isiahs-MacBook-Pro.local>
2022-07-11 10:26:01 -0600

    Updated viewModels

--- Summit/Views/TrailView/TrailCreationView/TrailCreationViewModel.swift
+++ Summit/Views/TrailView/TrailCreationView/TrailCreationViewModel.swift
@@ -10,13 +10,11 @@ import Foundation
 class TrailCreationViewModel {
 var trail: Trail?
 
-    init(trail: Trail? = nil){
-    self.trail = trail
-    }
+   
     func saveTrail(with name: String, entry: String, location: String, imageURL: String?, rating: Int, date: Date = Date(), distance: Double) {
-        if trail != nil {
+       
             // need to update exsiting trail, and also create a new trail saving it to firebase
             // also being able to get an image from storage in firebase
         }
-    }
+    
 }// end of class
